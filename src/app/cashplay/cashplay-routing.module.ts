9e07b4ec75009182f9c3ee0657ec4652fa123102
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-08-01 01:16:42 +0100

    workaround the problem with customer routing.

--- src/app/cashplay/cashplay-routing.module.ts
+++ src/app/cashplay/cashplay-routing.module.ts
@@ -1,30 +1,30 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 
-import {LayoutComponent} from './layout/layout.component';
-import {NavComponent} from './nav/nav.component';
-import {CustomerComponent} from './customer/customer/customer.component';
-import {CustomerHomeComponent} from './customer/customer-home/customer-home.component';
-import {HomeComponent} from './home/home/home.component';
+import {LayoutComponent} from "./layout/layout.component";
+import {NavComponent} from "./nav/nav.component";
+import {CustomerComponent} from "./customer/customer/customer.component";
+import {CustomerHomeComponent} from "./customer/customer-home/customer-home.component";
+import {HomeComponent} from "./home/home/home.component";
+import {SearchCustomerComponent} from "./customer/search-customer/search-customer.component";
+import {NewCustomerComponent} from "./customer/new-customer/new-customer.component";
 
 const routes: Routes = [
   {
     path: 'app', component: LayoutComponent, children: [
     {path: '', redirectTo: 'home', pathMatch: 'prefix'},
     {path: 'home', component: HomeComponent},
-    {path: 'customer', component: CustomerComponent}
+    {
+      path: 'customer', component: CustomerComponent, children: [
+      // FIXME put routes to customer module
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'home', component: CustomerHomeComponent},
+      {path: 'search', component: SearchCustomerComponent},
+      {path: 'new', component: NewCustomerComponent},
+    ]
+    }
   ]
   },
-  // {
-  //   path: 'app', component: LayoutComponent, children: [
-  //   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  //   {path: 'home', component: HomeComponent},
-  //   {path: 'customer', component: CustomerComponent, children: [
-  //     {path: 'home', component: CustomerHomeComponent}
-  //   ]}
-  //   // {path: '', redirectTo: 'customer', pathMatch: 'full'}
-  // ]
-  // },
 ];
 @NgModule({
   declarations: [
--- src/app/cashplay/customer/customer-routing.module.ts
+++ src/app/cashplay/customer/customer-routing.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 
-import { CustomerComponent } from './customer/customer.component';
-import { CustomerHomeComponent } from './customer-home/customer-home.component';
-import { SearchCustomerComponent } from './search-customer/search-customer.component';
-import { NewCustomerComponent } from './new-customer/new-customer.component';
+import {CustomerComponent} from "./customer/customer.component";
+import {CustomerHomeComponent} from "./customer-home/customer-home.component";
+import {SearchCustomerComponent} from "./search-customer/search-customer.component";
+import {NewCustomerComponent} from "./new-customer/new-customer.component";
 
 const routes: Routes = [
-  {path: 'customer', component: CustomerComponent, children: [
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'customer', component: CustomerComponent, children: [
+    // {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: CustomerHomeComponent},
+    {
+      path: '', children: [
+      {path: 'home', component: CustomerHomeComponent},
+      // {path: 'search', component: SearchCustomerComponent},
+      {path: 'new', component: NewCustomerComponent},
+    ]
+    },
+  ]
+  },
+  {
+    path: '', component: CustomerComponent, children: [
     {path: 'search', component: SearchCustomerComponent},
-    {path: 'new', component: NewCustomerComponent},
   ]},
 ];
 @NgModule({
@@ -21,4 +32,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CustomerRoutingModule {}
+export class CustomerRoutingModule {
+}
